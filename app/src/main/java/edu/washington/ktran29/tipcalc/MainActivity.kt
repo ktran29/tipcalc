75ab354d3a4343e2ba8959ee58105b73ce58b99d
Kevin Tran <ktran@tack.tech>
2018-04-16 01:21:52 -0700

    made text number only and add toast

--- app/src/main/java/edu/washington/ktran29/tipcalc/MainActivity.kt
+++ app/src/main/java/edu/washington/ktran29/tipcalc/MainActivity.kt
@@ -2,11 +2,43 @@ package edu.washington.ktran29.tipcalc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.text.TextWatcher
+import android.text.Editable
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var tipButton : Button? = null
+    private var tipText : EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tipText = findViewById(R.id.editText)
+        tipButton = findViewById(R.id.button)
+
+
+        tipText?.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                tipButton?.isEnabled = tipText?.text.toString().isNotEmpty()
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+        })
+
+        tipButton?.setOnClickListener { tipButtonClicked() }
+    }
+
+    private fun tipButtonClicked() {
+        Toast.makeText(this, tipText?.text.toString(), Toast.LENGTH_SHORT).show()
+        tipText?.setText("")
     }
 }
